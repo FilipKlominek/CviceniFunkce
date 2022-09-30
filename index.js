20e7b0dc982c7e567d2dc6a4f73ce17689c06efe
FilipKlominek <71869121+FilipKlominek@users.noreply.github.com>
2022-09-30 09:00:55 +0200

    1 file modified

--- index.js
+++ index.js
@@ -9,12 +9,22 @@ function minToSec(minutes) {
   return minutes * 60;
 }
 
-console.log(minToSec(3))
-console.log(minToSec(2))
+console.log(minToSec(3));
+console.log(minToSec(2));
 
 function aZaroven(a, b) {
   return a && b;
 }
 
-console.log(aZaroven(true, true))
-console.log(aZaroven(false, true))
+console.log(aZaroven(true, true));
+console.log(aZaroven(false, true));
+
+function area(x, y) {
+  if (x >= 0 && y >= 0) {
+    return x * y;
+  }
+  return -1;
+}
+
+console.log(area(4, 6));
+console.log(area(-3, 5));
